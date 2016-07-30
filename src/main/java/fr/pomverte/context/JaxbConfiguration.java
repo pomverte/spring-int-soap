6ce16f8c835c5040c6b72721d1665c5fae409017
pomverte <vietnem@yahoo.fr>
2016-07-30 18:20:54 +0200

    Consume SOAP with a Jaxb Element
    http://localhost:8080/jaxb

---
+++ src/main/java/fr/pomverte/context/JaxbConfiguration.java
@@ -0,0 +1,31 @@
+package fr.pomverte.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.xml.transformer.ResultToStringTransformer;
+import org.springframework.integration.xml.transformer.ResultTransformer;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import fr.pomverte.soap.ObjectFactory;
+
+@Configuration
+public class JaxbConfiguration {
+
+    @Bean
+    public ObjectFactory objectFactory() {
+        return new ObjectFactory();
+    }
+
+    @Bean
+    public ResultTransformer resultToStringTransformer() {
+        return new ResultToStringTransformer();
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("fr.pomverte.soap");
+        return marshaller;
+    }
+
+}
--- src/main/java/fr/pomverte/service/SoapBodyService.java
+++ src/main/java/fr/pomverte/service/SoapBodyService.java
@@ -1,14 +1,32 @@
 package fr.pomverte.service;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 
+import fr.pomverte.soap.FahrenheitToCelsius;
+import fr.pomverte.soap.ObjectFactory;
+
 @Component
 public class SoapBodyService {
+
+    @Autowired
+    private ObjectFactory objectFactory;
 
     @ServiceActivator
     public String stringSoapBody() {
         return "<FahrenheitToCelsius xmlns=\"http://www.w3schools.com/xml/\">" + "<Fahrenheit>90.0</Fahrenheit>"
                 + "</FahrenheitToCelsius>";
+    }
+
+    @ServiceActivator
+    public JAXBElement<FahrenheitToCelsius> jaxbSoapBody() {
+        FahrenheitToCelsius fahrenheitToCelsius = this.objectFactory.createFahrenheitToCelsius();
+        fahrenheitToCelsius.setFahrenheit("90.0");
+        return new JAXBElement<FahrenheitToCelsius>(new QName("http://www.w3schools.com/xml/", "FahrenheitToCelsius"),
+                FahrenheitToCelsius.class, fahrenheitToCelsius);
     }
 }
